457e4a093919c720dcf2343f5bf1f6d1d6aa3780
rodrigosilvab197 <rodrigosilvab197@gmail.com>
2019-08-21 02:36:30 +0000

    hello

---
+++ Makefile
@@ -0,0 +1,5 @@
+hello: hello.c
+	gcc hello.c -o hello
+test: hello
+	./hello fulanito parangano pepito juanito
+
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+    int main (int argc, char **argv) {
+        for( int i=1; i < argc ; i++){
+             printf("hello %s\n", argv[i]);
+        }
+
+        return 0;
+    }
